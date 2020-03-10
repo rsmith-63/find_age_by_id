6b5e123aaf763e2de1834974af8a43ef5cf05de2
rsmith-63 <rsmith_63@yahoo.com>
2020-03-10 15:45:21 -0400

    fix up repo

--- Autocomplete.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Autocomplete from './Autocomplete';
-
-it('Autocomplete renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Autocomplete />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
--- Results.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Results from './Results';
-
-it('Results renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Results />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
